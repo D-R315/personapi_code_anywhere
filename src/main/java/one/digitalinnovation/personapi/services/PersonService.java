358a5d0351f200f48c2526f8c4f96fd6ce12a686
Douglas Reis <dreis.gti@gmail.com>
2021-06-03 20:29:33 -0300

    Develop person update operation by ID

--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -3,6 +3,8 @@ package one.digitalinnovation.personapi.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,37 +30,53 @@ public class PersonService {
 		Person personToSave = personMapper.toModel(personDTO);
 		Person savedPerson = personRepository.save(personToSave);
 		
-		return MessageResponseDTO
-				.builder()
-				.message("Created person with ID " + savedPerson.getId())
-				.build();
+		return createMessageResponse(savedPerson.getId(), "Created person with ID ");
+		
 	}
-
-
+	
+	
 	public List<PersonDTO> listAll() {
 		List<Person> people = personRepository.findAll();
 		return people.stream()
 				.map(personMapper::toDTO)
 				.collect(Collectors.toList());
 	}
-
-
+	
+	
 	public PersonDTO findById(Long id) throws PersonNotFoundException {
 		Person person = verifyIfExists(id);
 		return personMapper.toDTO(person);
 	}
-
-
+	
+	
+	public MessageResponseDTO updateById(Long id, @Valid PersonDTO personDTO) throws PersonNotFoundException {
+		
+		verifyIfExists(id);
+		Person personToUpdate = personMapper.toModel(personDTO);
+		Person updatePerson = personRepository.save(personToUpdate);
+		
+		return createMessageResponse(updatePerson.getId(), "Update person with ID ");
+	}
+	
+	
 	public void delete(Long id) throws PersonNotFoundException {
 		verifyIfExists(id);
 		personRepository.deleteById(id);
 		
 	}
-
-
+	
+	
 	private Person verifyIfExists(Long id) throws PersonNotFoundException {
 		return personRepository.findById(id)
 				.orElseThrow(() -> new PersonNotFoundException(id));
 	}
-
+	
+	
+	private MessageResponseDTO createMessageResponse(Long id, String message) {
+		return MessageResponseDTO
+				.builder()
+				.message(message + id)
+				.build();
+	}
+	
 }
